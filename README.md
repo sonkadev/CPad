04e93f248c5289e4cfc1afe92043202fdb8442dc
sonka <sonkagamer1999@gmail.hu>
2023-09-30 15:04:12 +0200

    0.0.5

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1> CPAD </h1>
 <h2> a really bad text "editor" </h2>
-<h1> CHANGELOG (updates since 0.0.3)</h1>
-<h2> no bad things before saved text (yippee)</h2>
+<h1> CHANGELOG (updates somce 0.0.4) </h1>
+<h2> save as command added (**/saveas) 
+MORE LINES!!! </h2>
--- cpad.c
+++ cpad.c
@@ -3,29 +3,55 @@
 
 int main(){
     FILE *fl;
-
+    char inputmatrix[420][420];
+    int lines = 0;
     char input[420];
     char input2[420];
-    printf("CPAD V 0.0.4 \ntype **/commands for commands\nwrite something\n");
+    printf("CPAD V 0.0.5 \ntype **/commands for commands\nwrite something\n");
     int exit = 0;
     while (exit == 0){
-
+    char filename[260];
     gets(input2);
     FILE *fop;
-    
+    FILE *fl2;
 
     if (strcmp(input2, "**/commands") == 0){
-        printf("\n\n===commands\nprefix: **/\n**/save: it will save it as file.txt \n**/open: opens file.txt \n**/exit: i don't think i need to explain this\nmore commands coming soon!\n");
+        printf("\n\n===commands\nprefix: **/\n**/save: it will save it as file.txt \n**/open: opens file.txt \n**/exit: i don't think i need to explain this\n**/saveas: save as\nmore commands coming soon!\n");
     }
     else if (strcmp(input2, "**/save") == 0){
         
         fl = fopen("file.txt", "wt");
-
-        fprintf(fl, input);
+        int i;
+                for (i = 0; i < lines; i++)
+                {
+                fprintf(fl, inputmatrix[i]);
+                fprintf(fl, "\n");
+            }
+        
         fclose(fl);
 
         printf("\nsaved!\n");
 
+        
+    }
+    else if (strcmp(input2, "**/saveas") == 0){
+        
+        printf("\nenter file's name\n");
+        gets(filename);
+
+        
+        fl2 = fopen(filename, "wt");
+        int i;
+                for (i = 0; i < lines; i++)
+                {
+                fprintf(fl2, inputmatrix[i]);
+                fprintf(fl2, "\n");
+            }
+        
+        fclose(fl2);
+
+        printf("\nsaved as %s!\n", filename);
+       
     }
     else if (strcmp(input2, "**/exit") == 0){
         exit == 1;
@@ -40,6 +66,7 @@ int main(){
      else if (strcmp(input2, "**/clear") == 0){
          strcpy(input, " "); 
          strcpy(input2, " "); 
+         lines = 0;
         }
     /*else if (strcmp(input2, "*import")){
         fop = fopen("file.txt","rb");
@@ -48,6 +75,9 @@ int main(){
 */
     else{
         strcpy(input, input2); 
+        
+        strcpy(inputmatrix[lines], input2);
+        lines++;
     }
     
     }
